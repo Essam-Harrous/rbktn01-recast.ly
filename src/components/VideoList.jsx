397f3972293678fde124bb5f1b22b2e31e00de2e
essam <esamfarg91@gmail.com>
2019-12-10 20:39:06 +0100

    react

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,11 @@
-var VideoList = () => (
+import VideoListEntry from './VideoListEntry.js'
+import exampleVideoData from '../data/exampleVideoData.js'
+
+var VideoList = (props) => (
+
+
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {props.videos.map((videoObj)=> <VideoListEntry onClickHandler={props.onClickHandler} video={videoObj}/>)}
   </div>
 );
 
